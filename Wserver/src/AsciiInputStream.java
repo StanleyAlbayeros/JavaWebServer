1ddd0a5b60a7b44990bf1a0f07475cdd5aecc209
drkztan <drkztan@gmail.com>
2016-03-29 02:51:56 +0200

    formatting

--- Wserver/src/AsciiInputStream.java
+++ Wserver/src/AsciiInputStream.java
@@ -3,37 +3,32 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class AsciiInputStream extends FilterInputStream {
-	
+
 	private int caracter;
-	private boolean paso=false;
-	
-	protected AsciiInputStream (InputStream in)
-	{
+	private boolean paso = false;
+
+	protected AsciiInputStream(InputStream in) {
 		super(in);
-	} 
-	
-	
-	public int read() throws IOException { 	
+	}
+
+	public int read() throws IOException {
 		//
-		// metodo a heredar. tiramos la excepcion con throws 
-		// de la clase IOException que se encarga de notificar de problemas de i/o
-		
-		caracter= in.read();
-		if (caracter=='<')
-		{
-			paso=true;
+		// metodo a heredar. tiramos la excepcion con throws
+		// de la clase IOException que se encarga de notificar de problemas de
+		// i/o
+
+		caracter = in.read();
+
+		if (caracter == '<') {
+			paso = true;
 		}
-		if(!paso)
-		{
+		if (!paso) {
 			return caracter;
-		}
-		else
-		{
-			while (caracter!='>')
-			{
-					caracter= in.read();
+		} else {
+			while (caracter != '>') {
+				caracter = in.read();
 			}
-			paso=false;
+			paso = false;
 			return read();
 		}
 	}
--- Wserver/src/Hilo.java
+++ Wserver/src/Hilo.java
@@ -14,15 +14,12 @@ public class Hilo extends Thread {
 	String indexhtml = "index.html";
 
 	String respuestaError = "HTTP/1.1 404 Not Found\n\n" + "<!DOCTYPE><HTML><HEAD>PAGINA NO ENCONTRADA</HEAD></HTML>";
-	
+
 	String cabeceraOK = "HTTP/1.1 200 OK\n";
-	
+
 	static int BUFFER = 1024;
 
-	
-	
-	
-	String cabeceraPNG = cabeceraOK + "Content-Type: image/png\n" ; 
+	String cabeceraPNG = cabeceraOK + "Content-Type: image/png\n";
 
 	int caracter;
 
@@ -34,7 +31,8 @@ public class Hilo extends Thread {
 
 	public void run() {
 
-		InputStream is, is2, is3;;
+		InputStream is, is2, is3;
+		;
 		OutputStream os, os2;
 		System.out.println("Estamos ejecutando desde un thread!");
 
@@ -50,160 +48,150 @@ public class Hilo extends Thread {
 			System.out.println("Request recibida: " + br);
 
 			String filename = br.split("/")[1].split(" HTTP")[0].split("\\?")[0];
-			
-			
-			
+
 			String opciones = "";
 			try {
 				opciones = br.split("\\?")[1].split(" HTTP")[0];
 			} catch (ArrayIndexOutOfBoundsException e) {
 				e.printStackTrace();
 			}
-			
-			System.out.println("\n\n heeeeeeeeeey \n\n");		
-			
+
+			System.out.println("\n\n heeeeeeeeeey \n\n");
+
 			Boolean ASC = false;
 			Boolean ZIP = false;
 			Boolean GZIP = false;
 			Boolean PNG = false;
-			
-			//parsear opciones
-			//parsear opciones
-			//parsear opciones
-			//parsear opciones
-			
+
+			// parsear opciones
+			// parsear opciones
+			// parsear opciones
+			// parsear opciones
+
 			try {
-					
-					Pattern patronASC = Pattern.compile("asc=([^&]+)");
-					Pattern patronZIP = Pattern.compile("zip=([^&]+)");
-					Pattern patronGZIP = Pattern.compile("gzip=([^&]+)");
-					Pattern patronPNG = Pattern.compile("png=([^&]+)");
-					
-					Matcher ma = patronASC.matcher(opciones);
-					Matcher mz = patronZIP.matcher(opciones);
-					Matcher mg = patronGZIP.matcher(opciones);
-					Matcher mp = patronPNG.matcher(opciones);
-					
-					if (ma.find()){
-						ASC = Boolean.valueOf(ma.group(1));
-						System.out.println("\nOpción ASC habilitada\n");
-						
-					}
-					if (mz.find()){
-						ZIP = Boolean.valueOf(mz.group(1));
-						System.out.println("\nOpción ZIP habilitada\n");
 
-					}
-					if (mg.find()){
-						GZIP = Boolean.valueOf(mg.group(1));
-						System.out.println("\nOpción GZIP habilitada\n");
+				Pattern patronASC = Pattern.compile("asc=([^&]+)");
+				Pattern patronZIP = Pattern.compile("zip=([^&]+)");
+				Pattern patronGZIP = Pattern.compile("gzip=([^&]+)");
+				Pattern patronPNG = Pattern.compile("png=([^&]+)");
 
-					}
-					if (mp.find()){
-						PNG = Boolean.valueOf(mp.group(1));
-						System.out.println("\nOpción PNG habilitada\n");
+				Matcher ma = patronASC.matcher(opciones);
+				Matcher mz = patronZIP.matcher(opciones);
+				Matcher mg = patronGZIP.matcher(opciones);
+				Matcher mp = patronPNG.matcher(opciones);
 
-					}
-				} catch (PatternSyntaxException patternex){
+				if (ma.find()) {
+					ASC = Boolean.valueOf(ma.group(1));
+					System.out.println("\nOpción ASC habilitada\n");
+
+				}
+				if (mz.find()) {
+					ZIP = Boolean.valueOf(mz.group(1));
+					System.out.println("\nOpción ZIP habilitada\n");
+
+				}
+				if (mg.find()) {
+					GZIP = Boolean.valueOf(mg.group(1));
+					System.out.println("\nOpción GZIP habilitada\n");
+
+				}
+				if (mp.find()) {
+					PNG = Boolean.valueOf(mp.group(1));
+					System.out.println("\nOpción PNG habilitada\n");
+
+				}
+			} catch (PatternSyntaxException patternex) {
 				System.out.println("Error al parsear las opciones del HTTP request!!!\n");
 			}
-			//parsear opciones
-			//parsear opciones
-			//parsear opciones
-			//parsear opciones
+			
+			// parsear opciones
+			// parsear opciones
+			// parsear opciones
+			// parsear opciones
+			
 			String sirviendo = "Sirviendo " + filename;
-			String consoleLogASC = sirviendo  + " con opción ASC";
-			String consoleLogZIP = sirviendo  + " con opción ZIP";
-			String consoleLogGZIP = sirviendo  + " con opción GZIP";
-			String consoleLogPNG = sirviendo  + " con opción PNG";
-			
+			String consoleLogASC = sirviendo + " con opción ASC";
+			String consoleLogZIP = sirviendo + " con opción ZIP";
+			String consoleLogGZIP = sirviendo + " con opción GZIP";
+			String consoleLogPNG = sirviendo + " con opción PNG";
+
 			String ok = "servido!";
-			
+
 			String okASC = ok + "ASC";
 			String okZIP = ok + "ZIP";
 			String okGZIP = ok + "GZIP";
 			String okPNG = ok + "PNG";
 
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
 
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			
-			String cabeceraZip = cabeceraOK + "Content-Type: application/zip\n"
-					+ "Content-Disposition: filename=\"" +filename+ ".zip\"\n\n";
-			
+			String cabeceraZip = cabeceraOK + "Content-Type: application/zip\n" + "Content-Disposition: filename=\""
+					+ filename + ".zip\"\n\n";
+
 			String cabeceraHTML = cabeceraOK + "Content-Type: text/html\n" + "\n";
-			
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			//Cabeceras
-			
-			
-			
+
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
+			// Cabeceras
+
 			// filename= "index.html";
-			
+
 			System.out.println("Index of get: " + br.indexOf("GET"));
-			
+
 			if ((br.indexOf("GET") != -1)) {
 				System.out.println("Nos han pedido el archivo con filename: " + filename);
 				try {
-					
-					
-					if (ASC){
-						
+
+					if (ASC) {
+
 						System.out.println(consoleLogASC);
-						is3 = new FileInputStream(filename);					
+						is3 = new FileInputStream(filename);
 						is = new AsciiInputStream(is3);
 						os.write(cabeceraHTML.getBytes());
 						while ((caracter = is.read()) != -1) {
 							os.write(caracter);
 						}
-						
-						os.write("\r\n".getBytes());
-						//os.write(System.getProperty("line.separator").getBytes());
 
+						os.write("\n\n".getBytes(), 0, "\n\n".getBytes().length);
+						// os.write(System.getProperty("line.separator").getBytes());
 
 						System.out.println(okASC);
-						//os.flush();
-					} 
-					
-					if (ZIP){
-						
+						// os.flush();
+					}
+
+					if (ZIP) {
+
 						System.out.println(consoleLogZIP);
 						/////////////////
 						is2 = new FileInputStream(filename);
-						//os2 = clientSocket.getOutputStream();
-						
-						
+						// os2 = clientSocket.getOutputStream();
+
 						System.out.print(filename);
-						
-						
+
 						os2.write(cabeceraZip.getBytes());
 						zos.putNextEntry(new ZipEntry(filename));
 						byte buffer[] = new byte[BUFFER];
 						int length;
-						while ((length = is2.read(buffer)) >0){
-							zos.write(buffer,  0,  length);
+						while ((length = is2.read(buffer)) > 0) {
+							zos.write(buffer, 0, length);
 						}
-						os.write("\n\n".getBytes());
-//						zos.flush();
-						zos.closeEntry();
+						os2.write("\n\n".getBytes(), 0, "\n\n".getBytes().length);
+						// zos.flush();
+						// zos.closeEntry();
 
-//						zos.finish();
-//						zos.close();
-						
-						
-						
+						// zos.finish();
+						// zos.close();
+
 						System.out.println(okZIP);
 					}
-					
-					
+
 					if (!ASC) {
-						
+
 						System.out.println("Sirviendo " + filename + " tal cual");
 						is3 = new FileInputStream(filename);
 						os2.write(cabeceraHTML.getBytes());
@@ -211,19 +199,17 @@ public class Hilo extends Thread {
 							os2.write(caracter);
 						}
 					}
-					
 
-
-				}catch (FileNotFoundException excep) {
+				} catch (FileNotFoundException excep) {
 					os.write(respuestaError.getBytes());
 					excep.printStackTrace();
 				}
 			}
-			
+
 			is.close();
-//			zos.closeEntry();
-//			zos.close();
-//			clientSocket.close();
+			zos.closeEntry();
+			zos.close();
+			// clientSocket.close();
 
 		} catch (IOException excep) {
 			excep.printStackTrace();
