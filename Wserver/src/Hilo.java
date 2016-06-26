0c2c74e73f6205a6a7dfd2b9f49670fd4406de4f
drkztan <drkztan@gmail.com>
2016-06-26 20:15:10 +0200

    CASI REWORKED, HTML + PNG PARSING UP

--- Wserver/src/Hilo.java
+++ Wserver/src/Hilo.java
@@ -5,21 +5,16 @@ import java.util.zip.*;
 
 public class Hilo extends Thread {
 
-	Socket clientSocket;
 	ServerSocket serverSocket;
+	Socket clientSocket;
+	
 
 	FileInputStream is;
 
 	String br = null, filename;
 	String indexhtml = "index.html";
 
-	String respuestaError = "HTTP/1.1 404 Not Found\n\n" + "<!DOCTYPE><HTML><HEAD>PAGINA NO ENCONTRADA</HEAD></HTML>";
-
-	String cabeceraOK = "HTTP/1.1 200 OK\n";
-
 	static int BUFFER = 2048;
-
-	String cabeceraPNG = cabeceraOK + "Content-Type: image/png\n";
 
 	int caracter;
 
@@ -60,6 +55,7 @@ public class Hilo extends Thread {
 			Boolean ZIP = false;
 			Boolean GZIP = false;
 			Boolean PNG = false;
+			Boolean HTML = false;
 
 			// parsear opciones
 			// parsear opciones
@@ -71,33 +67,41 @@ public class Hilo extends Thread {
 				Pattern patronASC = Pattern.compile("asc=([^&]+)");
 				Pattern patronZIP = Pattern.compile("zip=([^&]+)");
 				Pattern patronGZIP = Pattern.compile("gzip=([^&]+)");
-				Pattern patronPNG = Pattern.compile("png=([^&]+)");
+				Pattern patronPNG = Pattern.compile(".png");
+				Pattern patronHTML = Pattern.compile(".html");
+				
+				Matcher matcherASC = patronASC.matcher(opciones);
+				Matcher matcherZIP = patronZIP.matcher(opciones);
+				Matcher matcherGZIP = patronGZIP.matcher(opciones);
+				Matcher matcherPNG = patronPNG.matcher(filename);
+				Matcher matcherHTML = patronHTML.matcher(filename);
 
-				Matcher ma = patronASC.matcher(opciones);
-				Matcher mz = patronZIP.matcher(opciones);
-				Matcher mg = patronGZIP.matcher(opciones);
-				Matcher mp = patronPNG.matcher(opciones);
-
-				if (ma.find()) {
-					ASC = Boolean.valueOf(ma.group(1));
+				if (matcherASC.find()) {
+					ASC = Boolean.valueOf(matcherASC.group(1));
 					System.out.println("\nOpción ASC habilitada\n");
 
 				}
-				if (mz.find()) {
-					ZIP = Boolean.valueOf(mz.group(1));
+				if (matcherZIP.find()) {
+					ZIP = Boolean.valueOf(matcherZIP.group(1));
 					System.out.println("\nOpción ZIP habilitada\n");
 
 				}
-				if (mg.find()) {
-					GZIP = Boolean.valueOf(mg.group(1));
+				if (matcherGZIP.find()) {
+					GZIP = Boolean.valueOf(matcherGZIP.group(1));
 					System.out.println("\nOpción GZIP habilitada\n");
 
 				}
-				if (mp.find()) {
-					PNG = Boolean.valueOf(mp.group(1));
+				if (matcherPNG.find()) {
+					PNG = Boolean.valueOf(matcherPNG.group(0));
 					System.out.println("\nOpción PNG habilitada\n");
 
 				}
+				if (matcherHTML.find()) {
+					HTML = Boolean.valueOf(matcherHTML.group(0));
+					System.out.println("\nOpción HTML habilitada\n");
+
+				}
+				
 			} catch (PatternSyntaxException patternex) {
 				System.out.println("Error al parsear las opciones del HTTP request!!!\n");
 			}
@@ -106,30 +110,57 @@ public class Hilo extends Thread {
 			// parsear opciones
 			// parsear opciones
 			// parsear opciones
+
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
 			
 			String sirviendo = "Sirviendo " + filename;
 			String consoleLogASC = sirviendo + " con opción ASC";
 			String consoleLogZIP = sirviendo + " con opción ZIP";
 			String consoleLogGZIP = sirviendo + " con opción GZIP";
-			String consoleLogPNG = sirviendo + " con opción PNG";
+			String consoleLogPNG = sirviendo + " como PNG";
+			String consoleLogHTML = sirviendo + " como HTML";
 
-			String ok = "servido!";
-
+			
+			String ok = "servido: ";
 			String okASC = ok + "ASC";
 			String okZIP = ok + "ZIP";
 			String okGZIP = ok + "GZIP";
 			String okPNG = ok + "PNG";
+			String okHTML = ok + "HTML";
+
+			
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
+			//Mensajes de consola para comprobar estados
 
 			// Cabeceras
 			// Cabeceras
 			// Cabeceras
 			// Cabeceras
 			// Cabeceras
+			
+			String cabeceraOK = "HTTP/1.1 200 OK\n";
+			
+			String cabeceraPNG = cabeceraOK + "Content-Type: image/png\n" + "Content-Disposition: filename=\""
+					+ filename + ".png\"\n\n";
 
 			String cabeceraZip = cabeceraOK + "Content-Type: application/zip\n" + "Content-Disposition: filename=\""
 					+ filename + ".zip\"\n\n";
 
-			String cabeceraHTML = cabeceraOK + "Content-Type: text/html\n" + "\n";
+			String cabeceraGZip = cabeceraOK + "Content-Type: application/gzip\n" + "Content-Disposition: filename=\""
+					+ filename + ".gz\"\n\n";
+			
+			String cabeceraHTML = cabeceraOK + "Content-Type: text/html\n"  + "Content-Disposition: filename=\""
+					+ filename + ".html\"\n\n";
+			
+			String respuestaError = "HTTP/1.1 404 Not Found\n\n" + "<!DOCTYPE><HTML><HEAD>Recurso: "
+					+ filename + " no encontrado</HEAD></HTML>"; 
 
 			// Cabeceras
 			// Cabeceras
